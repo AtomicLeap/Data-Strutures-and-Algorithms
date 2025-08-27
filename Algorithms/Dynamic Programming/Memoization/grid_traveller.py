9ff9cf7e9f0421d5cbc9812d355f1f9306056a14
atomicLeap <stanleyagwu2023@gmail.com>
2025-08-27 21:09:22 +0200

    feat(algorithms): Dynamic Programming - memoization - Grid traveller - Python

--- Algorithms/Dynamic Programming/Memoization/grid_traveller.py
+++ Algorithms/Dynamic Programming/Memoization/grid_traveller.py
@@ -1,6 +1,15 @@
 # Grid Traveller
 # How many ways can a traveller travel to the bottom right end in an m x n grid
 
+"""
+Say that you are a traveller on a 2D grid. You begin in the top-left corner 
+and your goal is to travel to the bottom-right corner. You may only move
+down or right. In how many ways can you travel to the goal on a grid with
+dimensions m * n?
+
+Write a function 'grid_traveller(m, n)' that calculates this.
+"""
+
 # Brute force solution
 def grid_traveller(n: int, m: int) -> int:
     if m == 1 and n == 1:
