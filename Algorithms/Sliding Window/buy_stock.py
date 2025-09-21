3794d8473bdf0073ee16801691fb30e64d32361a
atomicLeap <stanleyagwu2023@gmail.com>
2025-09-21 20:57:38 +0200

    feat(algorithms): Sliding window - best time to buy and sell stocks - Python

---
+++ Algorithms/Sliding Window/buy_stock.py
@@ -0,0 +1,71 @@
+# Leetcode 121. Best Time to Buy and Sell Stock
+
+"""
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+"""
+
+# Sliding Window Approach
+def buy_stock(prices: list[int]) -> int:
+    buy, sell, n, max_profit = 0 , 1, len(prices), 0
+
+    while sell < n:
+        if prices[sell] > prices[buy]:
+            profit = prices[sell] - prices[buy]
+            max_profit = max(max_profit, profit)
+        else:
+            buy = sell
+        sell += 1
+    return max_profit
+
+# n = len(prices)
+# O(n) - Time complexity
+# O(1) - Space complexity
+
+print(buy_stock([7,1,5,3,6,4])) # 5
+print(buy_stock([7,6,4,3,1])) # 0
+print(buy_stock([3, 3])) # 0
+print(buy_stock([2, 1, 4])) # 3
+
+def buy_sell_stock(prices: list[int]) -> int:
+    min_price, max_profit, n = float('inf'), 0, len(prices)
+
+    for i in range(n):
+        if prices[i] < min_price:
+            min_price = prices[i]
+        else:
+            profit = prices[i] - min_price
+            max_profit = max(max_profit, profit)
+    return max_profit
+
+# n = len(prices)
+# O(n) - Time complexity
+# O(1) - Space complexity
+
+print(buy_sell_stock([7,1,5,3,6,4])) # 5
+print(buy_sell_stock([7,6,4,3,1])) # 0
+print(buy_sell_stock([3, 3])) # 0
+print(buy_sell_stock([2, 1, 4])) # 3
