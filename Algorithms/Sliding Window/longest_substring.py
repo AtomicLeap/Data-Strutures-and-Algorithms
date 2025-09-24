0530b64c987b4c00bc950ed7340eeaaa831e3c31
atomicLeap <stanleyagwu2023@gmail.com>
2025-09-24 22:04:04 +0200

    feat(algorithms): Sliding window - longest substring without repeating character - Python

--- Algorithms/Sliding Window/longest_substring.py
+++ Algorithms/Sliding Window/longest_substring.py
@@ -1,6 +1,45 @@
 # Leetcode 3. Longest Substring Without Repeating Characters
 
 """
+Given a string s, find the length of the longest substring without duplicate characters.
+
+Example 1:
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+"""
+
+def longest_sub_string(s: str) -> int:
+    total, left, visited = 0, 0, {}
+
+    for right, char in enumerate(s):
+        if char in visited and visited[char] >= left:
+            left = visited[char] + 1
+        visited[char] = right
+        total = max(total, right - left + 1)
+    return total
+
+# O(n) Time complexity
+# O(n) Space complexity
+
+print(longest_sub_string('abcabcbb')) # 3
+print(longest_sub_string('abcabcbdhkmvebhgkldgg')) # 9
+print(longest_sub_string('abcdefabctyuimnbcbb')) # 12
+print(longest_sub_string('abcdefghijkl')) # 12
+print(longest_sub_string('aaaaaaaaaaaaaa')) # 1
+
+"""
 Given a string s, find the longest substring without repeating characters.
 Write a function `longest_substring` that takes a string s, and returns 
 the longest sub-string.
