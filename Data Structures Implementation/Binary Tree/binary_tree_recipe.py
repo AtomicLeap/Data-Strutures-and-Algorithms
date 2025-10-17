f55ded7e00e9e90a24be32abb25809a97dfbcc7f
atomicLeap <stanleyagwu2023@gmail.com>
2025-10-17 20:20:38 +0200

    feat(data structures implementation): binary tree and bst recipe - updated - Python

--- Data Structures Implementation/Binary Tree/binary_tree_recipe.py
+++ Data Structures Implementation/Binary Tree/binary_tree_recipe.py
@@ -1,12 +1,26 @@
-# Binary Tree Recipe
+# Binary Tree and Binary Search Tree Recipe
 
-# Three conditions that makes a tree, a binary tree are: A binary tree must have:
+# Binary Tree
+
+# Three conditions make a tree a binary tree. These are, A binary tree must have:
 
 # 1. Only one (1) root node.
 
 # 2. At most two (2) children per node.
 
 # 3. Exactly one (1) path between the root and any node.
+
+
+# Binary Search Tree
+
+# Three conditions make a tree a valid BST. These are defined as follows:
+
+# 1. The left subtree of a node contains only nodes with keys strictly less than the node's key.
+
+# The right subtree of a node contains only nodes with keys strictly greater than the node's key.
+
+# Both the left and right subtrees must also be binary search trees.
+
 
 
 # Binary Tree Node class
