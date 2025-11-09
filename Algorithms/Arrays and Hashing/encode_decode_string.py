cb7b6ebea26c39401864f429cb78d8e46f2b9ce6
atomicLeap <stanleyagwu2023@gmail.com>
2025-11-09 15:35:44 +0100

    feat(alogorithms): Arrays and Hashing - Encode and Decode Strings - Python

--- Algorithms/Arrays and Hashing/encode_decode_string.py
+++ Algorithms/Arrays and Hashing/encode_decode_string.py
@@ -10,7 +10,7 @@ class Codec:
         return ''.join(f'{len(s)}#{s}' for s in strs)
 
     def decode(self, s: str) -> list[str]:
-        res, i, n = [], 0, len(s)
+        result, i, n = [], 0, len(s)
         while i < n:
             # read length up to '#'
             j = i
@@ -18,9 +18,12 @@ class Codec:
                 j += 1
             length = int(s[i:j])
             j += 1  # skip '#'
-            res.append(s[j:j+length])
+            result.append(s[j:j+length])
             i = j + length
-        return res
+        return result
+
+# O(n) Time complexity
+# O(n) Space complexity
 
 codec = Codec()
 
