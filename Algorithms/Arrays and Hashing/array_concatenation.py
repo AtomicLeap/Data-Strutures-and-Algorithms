b40b23fde2a4d86df8eec9cd441db29a5e968645
atomicLeap <stanleyagwu2023@gmail.com>
2025-11-10 17:46:26 +0100

    feat(algorithms): Arrays and Hashing - Concatenation of Array - Python

---
+++ Algorithms/Arrays and Hashing/array_concatenation.py
@@ -0,0 +1,9 @@
+# Leetcode 1929. Concatenation of Array
+
+# https://leetcode.com/problems/concatenation-of-array/description/
+
+def array_concatenation(nums: list[int]) -> list[int]:
+    return nums.extend(nums)
+
+print(array_concatenation([1,2,1])) # [1,2,1,1,2,1]
+print(array_concatenation([1,3,2,1])) # [1,3,2,1,1,3,2,1]
