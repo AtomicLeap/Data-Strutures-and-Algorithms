a00bc258120502322b49329251d5529dc21534db
atomicLeap <stanleyagwu2023@gmail.com>
2025-11-11 20:17:57 +0100

    feat(algorithms): Arrays and Hashing - Array rotation - Python

--- Algorithms/Arrays and Hashing/array_rotation.py
+++ Algorithms/Arrays and Hashing/array_rotation.py
@@ -8,23 +8,31 @@ def rotate_right(arr, k):
     k %= n  # wrap k within [0, n]
     return [arr[(i - k) % n] for i in range(n)]
 
+# O(n) Time complexity
+# O(n) Space complexity
+
 # B) In-place (no extra array)
-def _rev(a, l, r):
+def _rev(arr, l, r):
     while l < r:
-        a[l], a[r] = a[r], a[l]
+        arr[l], arr[r] = arr[r], arr[l]
         l += 1; r -= 1
 
-def rotate_right_inplace(a, k):
-    n = len(a)
+def rotate_right_inplace(arr, k):
+    n = len(arr)
     if n == 0: 
-        return
-    k %= n
-    _rev(a, 0, n - 1)
-    _rev(a, 0, k - 1)
-    _rev(a, k, n - 1)
+        return arr
+    k %= n # Normalize rotation count
+    _rev(arr, 0, n - 1) # Step 1: reverse entire array
+    _rev(arr, 0, k - 1) # Step 2: reverse first k elements
+    _rev(arr, k, n - 1) # Step 3: reverse the rest
 
+    return arr
 
+# O(n) Time complexity
+# O(1) Space complexity
 
-print(rotate_right(["a", "b", "c", "d", "e", "f"], 3))
-print(rotate_right(["a", "b", "c", "d", "e", "f"], 28))
-# ["c", "d", "e", "f"]
+print(rotate_right(["a", "b", "c", "d", "e", "f"], 3)) # ['d', 'e', 'f', 'a', 'b', 'c']
+print(rotate_right(["a", "b", "c", "d", "e", "f"], 28)) # ['c', 'd', 'e', 'f', 'a', 'b']
+
+print(rotate_right_inplace(["a", "b", "c", "d", "e", "f"], 3)) # ['d', 'e', 'f', 'a', 'b', 'c']
+print(rotate_right_inplace(["a", "b", "c", "d", "e", "f"], 28)) # ['c', 'd', 'e', 'f', 'a', 'b']
