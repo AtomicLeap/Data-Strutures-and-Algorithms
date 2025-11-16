f071f413471245a4e4fc5058e439c0fe2f9079e5
atomicLeap <stanleyagwu2023@gmail.com>
2025-11-16 22:59:42 +0100

    feat(algorithms): Graph - has path problem - Python

--- Algorithms/Graphs/has_path.py
+++ Algorithms/Graphs/has_path.py
@@ -1,11 +1,6 @@
 # Graph has path problem
 
-"""
-Write a function, hasPath, that takes in an object representing the 
-adjacency list of a directed acyclic graph and two nodes (src, dst). 
-The function should return a boolean indicating whether or not there 
-exists a directed path between the source and destination nodes.
-"""
+# https://structy.net/problems/has-path
 
 # Graph is acyclic - No cycle - This helps to prevent infinite loop
 graph: dict[str, list[str]] = {
