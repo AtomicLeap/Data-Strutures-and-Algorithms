a32251ac1fe47045761ea121558aa5c7941c802d
atomicLeap <stanleyagwu2023@gmail.com>
2025-11-17 21:43:20 +0100

    feat(algorithms): Graphs - minimum island - Python

--- Algorithms/Graphs/minimum_island.py
+++ Algorithms/Graphs/minimum_island.py
@@ -1,13 +1,6 @@
 # minimum island
 
-"""
-Write a function, minimumIsland, that takes in a grid containing Ws and Ls. 
-W represents water and L represents land. The function should return the 
-size of the smallest island. An island is a vertically or horizontally 
-connected region of land.
-
-You may assume that the grid contains at least one island.
-"""
+# https://structy.net/problems/minimum-island
 
 grid_1 = [
   ['W', 'L', 'W', 'W', 'W'],
