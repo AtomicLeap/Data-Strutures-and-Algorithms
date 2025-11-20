30105515567218600bbdb7638b68e8afacf7c1bb
atomicLeap <stanleyagwu2023@gmail.com>
2025-11-20 23:18:29 +0100

    feat(algorithms): Graphs - shortest path - Python

--- Algorithms/Graphs/shortest_path.py
+++ Algorithms/Graphs/shortest_path.py
@@ -1,12 +1,6 @@
 # shortest path
 
-"""
-Write a function, shortestPath, that takes in an array of edges for an undirected 
-graph and two nodes (nodeA, nodeB). The function should return the length of the 
-shortest path between A and B. Consider the length as the number of edges in the 
-path, not the number of nodes. If there is no path between A and B, then return -1. 
-You can assume that A and B exist as nodes in the graph.
-"""
+# https://structy.net/problems/shortest-path
 
 # NOTE: For undirected graphs, You will always need a visited list/set/dict to 
 # keep tract of visited nodes. This is to avoid being trapped in infinite loop.
