b3805be5bb51612c4e5c919ce729b5fdb8158385
atomicLeap <stanleyagwu2023@gmail.com>
2025-11-21 21:51:35 +0100

    feat(data structures implementation): Graphs - Breadth First Traversal - Python

--- Data Structures Implementation/Graph/breadth_first_traversal.py
+++ Data Structures Implementation/Graph/breadth_first_traversal.py
@@ -13,11 +13,14 @@ graph: dict[str, list[str]] = {
     "f": []
 }
 # Iteractive method (only)
+
+from collections import deque
+
 def breadth_first_print(adjacency_list: dict[str, list[str]], source_node: str):
-    queue = [ source_node ]
+    queue = deque([ source_node ]) # O(1) Pop left and right, unlike list with O(n) Pop left
 
     while queue:
-        current = queue.pop(0)
+        current = queue.popleft()
         print(current)
 
         for neighbour in adjacency_list[current]:
