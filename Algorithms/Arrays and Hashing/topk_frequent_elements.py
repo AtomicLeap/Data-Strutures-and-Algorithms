0251f1c1199101348f092897daaf440365602772
atomicLeap <stanleyagwu2023@gmail.com>
2025-12-12 23:49:38 +0100

    feat(algorithms): Arrays and Hashing - Top K Frequent Elements - Python

--- Algorithms/Arrays and Hashing/topk_frequent_elements.py
+++ Algorithms/Arrays and Hashing/topk_frequent_elements.py
@@ -5,26 +5,26 @@
 
 from collections import Counter
 
-# Bucket sort solution
+# Prefix Frequency Array solution
 def topk_frequent_elements(nums: list[int], k: int) -> list[int]:
     # 1) Frequency map
     frequencies = Counter(nums)   # O(n)
 
-    # 2) Buckets: index = frequency, value = list of numbers with that frequency
+    # 2) Prefix Freq. Array: index = frequency, value = list of numbers with that frequency
     n = len(nums)
-    buckets = [[] for _ in range(n + 1)]  # frequencies are in [0, 1..n]
+    prefix_freq = [[] for _ in range(n + 1)]  # frequencies are in [0, 1..n]
     for num, freq in frequencies.items():           # O(U) ⊆ O(n)
-        buckets[freq].append(num)
+        prefix_freq[freq].append(num)
 
     # 3) Gather from highest frequency down
-    answer = []
-    for f in range(n, 0, -1):             # O(n)
-        if buckets[f]:
-            answer.extend(buckets[f])
-            if len(answer) >= k:
-                return answer[:k] # constraints guarantee we return earlier
+    results = []
+    for idx in range(n, 0, -1):             # O(n)
+        if prefix_freq[idx]:
+            results.extend(prefix_freq[idx])
+            if len(results) >= k:
+                return results[:k] # constraints guarantee we return earlier
 
-    return answer  # fallback
+    return results  # fallback
 
 # O(n) Time complexity -> (counting + filling buckets + single pass over buckets)
 # O(n) Space complexity -> for buckets and frequency map
@@ -56,3 +56,20 @@ def topk_frequent_heap(nums: list[int], k: int) -> list[int]:
 print(topk_frequent_heap([1,1,1,2,2,3,3,3,3,3], 2)) # [3, 1]
 print(topk_frequent_heap([1,1,1,2,2,3], 2)) # [1, 2]
 print(topk_frequent_heap([1,2,1,2,1,2,3,1,3,2], 2)) # [1, 2]
+
+from collections import Counter
+
+def topk_frequent(nums: list[int], k:int) -> list[int]:
+    counts = Counter(nums)
+    sorted_counts = sorted(((num, freq) for (num, freq) in counts.items()), \
+                     key=lambda item: (-item[1], item[0]))
+    results = [num for (num, _) in sorted_counts]
+    
+    return results[:k]
+
+# O(n log n) Time complexity
+# O(n) Space complexity
+
+print(topk_frequent([1,1,1,2,2,3,3,3,3,3], 2)) # [3, 1]
+print(topk_frequent([1,1,1,2,2,3], 2)) # [1, 2]
+print(topk_frequent([1,2,1,2,1,2,3,1,3,2], 2)) # [1, 2]
