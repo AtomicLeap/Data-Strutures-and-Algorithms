289818f867c170291d1dbe0e2705888e57170d0d
atomicLeap <stanleyagwu2023@gmail.com>
2026-01-01 23:45:22 +0100

    feat(algorithms): Bit Manipulation - Find the Difference - Python

---
+++ Algorithms/Bit Manipulation/find_difference.py
@@ -0,0 +1,41 @@
+# Leetcode 389. Find the Difference
+
+# https://leetcode.com/problems/find-the-difference/description/
+
+# XOR trick
+def find_difference(s: str, t: str) -> str:
+    diff = 0
+
+    for char in s:
+        diff ^= ord(char)
+
+    for char in t:
+        diff ^= ord(char)
+
+    return chr(diff)
+
+# O(n) Time complexity
+# O(1) Space complexity
+
+print(find_difference('abcd', 'adecb')) # 'e'
+print(find_difference('', 'y')) # 'y'
+
+# Tabulation - Since we are only concerned with alphabets
+def find_difference_t(s: str, t: str) -> str:
+    table = [0] * 26
+
+    for char in t:
+        table[ord(char) - ord('a')] += 1
+    
+    for char in s:
+        table[ord(char) - ord('a')] -= 1
+    
+    for idx, value in enumerate(table):
+        if value == 1:
+            return chr(idx + ord('a'))
+        
+# O(n) Time complexity
+# O(1) Space complexity
+
+print(find_difference_t('abcd', 'adecb')) # 'e'
+print(find_difference_t('', 'y')) # 'y'
