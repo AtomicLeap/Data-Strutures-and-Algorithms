a2fd1ff0dcf72f074a99ecea1c0849efd188684d
atomicLeap <stanleyagwu2023@gmail.com>
2026-01-01 23:44:11 +0100

    feat(algorithms): Bit Manipulation - Single Number III - Python

---
+++ Algorithms/Bit Manipulation/single_number_III.py
@@ -0,0 +1,32 @@
+# Leetcode 260. Single Number III
+
+# https://leetcode.com/problems/single-number-iii/description/
+
+def single_number(nums: list[int]) -> list[int]:
+    # Step 1: XOR of all elements = x ^ y
+    xor_result = 0
+
+    for num in nums:
+        xor_result ^= num
+
+    # Step 2: isolate rightmost set bit
+    diff_bit = xor_result & -xor_result
+
+    # Step 3: partition to 2 groups and XOR
+    single_1 = 0
+    single_2 = 0
+
+    for num in nums:
+        if num & diff_bit:
+            single_1 ^= num
+        else:
+            single_2 ^= num
+    return [single_1, single_2]
+
+# O(n) Time complexity
+# O(1) Space complexity
+
+
+print(single_number([1,2,1,3,2,5])) # [3, 5]
+print(single_number([-1,0])) # [-1,0]
+print(single_number([0,1])) # [1, 0]
