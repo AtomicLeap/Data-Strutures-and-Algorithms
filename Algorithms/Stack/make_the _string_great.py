a1a08d58e45d38ed34daef5f7d1155ac2e3938fd
atomicLeap <stanleyagwu2023@gmail.com>
2026-01-11 10:20:25 +0100

    feat(algorithms): Stack - Make The String Great - Python

--- Algorithms/Stack/make_the _string_great.py
+++ Algorithms/Stack/make_the _string_great.py
@@ -1,39 +1,15 @@
-# Make The String Great
+# Leetcode 1544. Make The String Great
 
-# Data structure - Stack
-
-"""
-Given a string s of lower and upper case English letters.
-
-A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
-
-0 <= i <= s.length - 2
-s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
-To make the string good, you can choose two adjacent characters that make the string bad and 
-remove them. You can keep doing this until the string becomes good.
-
-Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
-Notice that an empty string is also good.
-Input: s = "abBAcC"
-Output: ""
-
-Input: s = "leEeetcode"
-Output: "leetcode"
-
-Input: s = "s"
-Output: "s"
-"""
+# https://leetcode.com/problems/make-the-string-great/description/
 
 def make_great_str(s: str) -> str:
-    if not s:
-        return ''
     stack = []
 
-    for i in range(len(s)):
-        if stack and stack[-1].lower() == s[i].lower() and stack[-1] != s[i]:
+    for char in s:
+        if stack and stack[-1] != char and stack[-1].lower() == char.lower() :
             stack.pop()
         else:
-            stack.append(s[i])
+            stack.append(char)
     return "".join(stack)
 
 # n = len(s)
