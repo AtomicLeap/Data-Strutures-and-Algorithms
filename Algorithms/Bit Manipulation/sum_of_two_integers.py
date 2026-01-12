d89822c27f5d0bca8d4927ff60ecbdc9dd5b40da
atomicLeap <stanleyagwu2023@gmail.com>
2026-01-12 23:45:48 +0100

    feat(algorithms): Bit Manipulation - Sum of Two Integers - Python

--- Algorithms/Bit Manipulation/sum_of_two_integers.py
+++ Algorithms/Bit Manipulation/sum_of_two_integers.py
@@ -1,19 +1,6 @@
 # Leetcode 371. Sum of Two Integers
 
-"""
-Given two integers a and b, return the sum of the two integers without using the operators + and -.
-
-Example 1:
-Input: a = 1, b = 2
-Output: 3
-
-Example 2:
-Input: a = 2, b = 3
-Output: 5
- 
-Constraints:
--1000 <= a, b <= 1000
-"""
+# https://leetcode.com/problems/sum-of-two-integers/description/
 
 def sum_of_ints(a: int, b: int) -> int:
     # 32-bit mask in Python (since Python ints are unbounded)
