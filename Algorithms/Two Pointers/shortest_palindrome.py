5b3c2d4e01c3e6384efec107ac9a0ba964851ffd
atomicLeap <stanleyagwu2023@gmail.com>
2026-01-20 21:38:26 +0100

    feat(Algorithms): Two Pointers - Shortest Palindrome - Python

--- Algorithms/Two Pointers/shortest_palindrome.py
+++ Algorithms/Two Pointers/shortest_palindrome.py
@@ -22,27 +22,28 @@ def shortest_palindrome(s: str) -> str:
     rev = s[::-1]
     # Build the combined string: original + '#' + reversed
     t = s + '#' + rev
+    n = len(t)
     
     # Compute Longest Prefix-Suffix (LPS) match array for t
-    lps = [0] * len(t)
+    longest_prefix_suffix_match = [0] * n
     
     # length of the current longest prefix-suffix
     length = 0
     
-    # We start from i = 1 because lps[0] is always 0
-    for i in range(1, len(t)):
+    # We start from i = 1 because longest_prefix_suffix_match[0] is always 0
+    for i in range(1, n):
         # Move length back while we don't have a match
         while length > 0 and t[i] != t[length]:
-            length = lps[length - 1]
+            length = longest_prefix_suffix_match[length - 1]
         
         # If characters match, extend the current prefix-suffix
         if t[i] == t[length]:
             length += 1
-            lps[i] = length
-        # else lps[i] stays 0
+            longest_prefix_suffix_match[i] = length
+        # else longest_prefix_suffix_match[i] stays 0
     
-    # lps[-1] is the length of the longest prefix of s that is a palindrome
-    longest_pref_suffix_match = lps[-1]
+    # longest_prefix_suffix_match[-1] is the length of the longest prefix of s that is a palindrome
+    longest_pref_suffix_match = longest_prefix_suffix_match[-1]
     
     # Characters after this prefix suffix match need to be mirrored in front
     suffix_to_add = s[longest_pref_suffix_match:]
@@ -50,6 +51,8 @@ def shortest_palindrome(s: str) -> str:
     # Reverse suffix_to_add and concat with string
     return suffix_to_add[::-1] + s
 
+# O(n) - Time complexity
+# O(n) - Space complexity -> extra space from slicing
 
 print(shortest_palindrome("aacecaaa")) # "aaacecaaa"
 print(shortest_palindrome("abcd")) # "dcbabcd"
