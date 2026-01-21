79b6502049eb0d4a43bc0b0ec75d576197909a75
atomicLeap <stanleyagwu2023@gmail.com>
2026-01-21 23:19:10 +0100

    feat(Algorithms): Intervals - Merge Intervals - Python

--- Algorithms/Intervals/merge_intervals.py
+++ Algorithms/Intervals/merge_intervals.py
@@ -1,30 +1,6 @@
 # Leetcode 56. Merge Intervals
 
-"""
-Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, 
-and return an array of the non-overlapping intervals that cover all the intervals in the input.
-
-Example 1:
-Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
-Output: [[1,6],[8,10],[15,18]]
-Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
-
-Example 2:
-Input: intervals = [[1,4],[4,5]]
-Output: [[1,5]]
-Explanation: Intervals [1,4] and [4,5] are considered overlapping.
-
-Example 3:
-Input: intervals = [[4,7],[1,4]]
-Output: [[1,7]]
-Explanation: Intervals [1,4] and [4,7] are considered overlapping.
- 
-
-Constraints:
-1 <= intervals.length <= 104
-intervals[i].length == 2
-0 <= starti <= endi <= 104
-"""
+# https://leetcode.com/problems/merge-intervals/description/
 
 def merge_intervals(intervals: list[list[int, int]]) -> list[list[int, int]]:
     intervals.sort(key=lambda x: x[0])
@@ -33,8 +9,9 @@ def merge_intervals(intervals: list[list[int, int]]) -> list[list[int, int]]:
     for start, end in intervals[1:]:
         last_start, last_end = results[-1]
         if start <= last_end: # there is an overlap - including touching
-            end = max(last_end, end)
-            results[-1] = [last_start, end]
+            new_start = min(last_start, start) # Optional, since intervals sorted already
+            new_end = max(last_end, end)
+            results[-1] = [new_start, new_end]
         else:
             results.append([start, end])
     return results
