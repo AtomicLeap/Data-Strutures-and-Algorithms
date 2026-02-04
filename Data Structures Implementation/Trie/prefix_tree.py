f789625347b208fed31328c63fe5a35aa39dba93
atomicLeap <stanleyagwu2023@gmail.com>
2026-02-04 23:20:45 +0100

    feat(Data Structures Implementation): Trie - Implement Trie (Prefix Tree) - Python

--- Data Structures Implementation/Trie/prefix_tree.py
+++ Data Structures Implementation/Trie/prefix_tree.py
@@ -2,6 +2,9 @@
 
 # https://leetcode.com/problems/implement-trie-prefix-tree/description/
 
+
+# Use HashMap per Node
+
 class TrieNode:
     def __init__(self):
         self.children = {} # 26 letters of alphabets
@@ -37,3 +40,47 @@ class Trie:
                 return False
             current = current.children[char]
         return True
+
+   
+# Use Fixed 26-Array per Node
+
+class TrieNodeA:
+    def __init__(self):
+        # Fixed array of 26 children
+        self.children = [None] * 26
+        self.is_word_end = False
+
+class TrieA:
+    def __init__(self):
+        self.root = TrieNodeA()
+
+    def insert(self, word: str) -> None:
+        current = self.root
+
+        for char in word:
+            idx = ord(char) - ord('a')
+            if not current.children[idx]:
+                current.children[idx] = TrieA()
+            current = current.children[idx]
+        current.is_word_end = True
+    
+    def search(self, word: str) -> bool:
+        current = self.root
+
+        for char in word:
+            idx = ord(char) - ord('a')
+            if not current.children[idx]:
+                return False
+            current = current.children[idx]
+        return current.is_word_end
+    
+    def starts_with(self, word: str) -> bool:
+        current = self.root
+
+        for char in word:
+            idx = ord(char) - ord('a')
+            if not current.children[idx]:
+                return False
+            current = current.children[idx]
+        return True
+
