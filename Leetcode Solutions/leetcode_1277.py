1c24e6111d28259b5f18700bbee0ac2320a6fd8c
atomicLeap <stanleyagwu2023@gmail.com>
2026-03-20 23:28:24 +0100

    feat(Leetcode Solutions): Count Square Submatrices with All Ones - updated - Python

--- Leetcode Solutions/leetcode_1277.py
+++ Leetcode Solutions/leetcode_1277.py
@@ -9,28 +9,28 @@ def solution(matrix: list[list[int]]) -> int:
         return 0
     
     m, n = len(matrix), len(matrix[0])
-    total = 0
+    total = 0 # total of cells with ones "1"
 
     # First row and first column stay as-is (0 or 1), already represent dp values
-    for i in range(m):
-        for j in range(n):
-            if matrix[i][j] == 1 and i > 0 and j > 0:
-                matrix[i][j] = 1 + min(matrix[i-1][j], matrix[i][j-1], matrix[i-1][j-1])
-            # add dp[i][j] to total
-            total += matrix[i][j]
+    for row in range(m):
+        for col in range(n):
+            if matrix[row][col] == 1 and row > 0 and col > 0:
+                matrix[row][col] = 1 + min(matrix[row-1][col], matrix[row][col-1], matrix[row-1][col-1])
+            # add matrix[row][col] to total
+            total += matrix[row][col]
     return total
 
 print(solution([
   [0,1,1,1],
   [1,1,1,1],
   [0,1,1,1]
-]))
+])) # 15
 
 print(solution([
   [1,0,1],
   [1,1,0],
   [1,1,0]
-]))
+])) # 7
 
 # Let m, n = len(matrix), len(matrix[0])
 # O(m . n) - Time complexity
