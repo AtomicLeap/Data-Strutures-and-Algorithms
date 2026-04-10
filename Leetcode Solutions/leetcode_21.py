887352b4173622c617be7c7a33334e050c436c2c
atomicLeap <stanleyagwu2023@gmail.com>
2026-04-10 23:26:22 +0200

    feat(Leetcode Solutions): Merge Two Sorted Lists - Python

--- Leetcode Solutions/leetcode_21.py
+++ Leetcode Solutions/leetcode_21.py
@@ -4,25 +4,25 @@
 
 # Tags -> Linked list
 
-# LeetCode-style ListNode:
+# ListNode:
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 def solution(list1: list[ListNode], list2: list[ListNode]) -> ListNode:
-    dummy = tail = ListNode()
-    a, b = list1, list2
-    while a and b:
-        if a.val <= b.val:
-            tail.next = a
-            a = a.next
+    head = ListNode()
+    current = head
+    while list1 and list2:
+        if list1.val <= list2.val:
+            current.next = list1
+            list1 = list1.next
         else:
-            tail.next = b
-            b = b.next
-        tail = tail.next
-    tail.next = a or b
-    return dummy.next
+            current.next = list2
+            list2 = list2.next
+        current = current.next
+    current.next = list1 or list2
+    return head.next
 
 
 # a, b = [1,2,4], [1,3,4]
